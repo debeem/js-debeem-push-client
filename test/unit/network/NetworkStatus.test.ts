abbcb97ddb0cca498682b8d9ea83f96563b418c4
deframework <deframework@outlook.com>
2024-12-04 01:20:06 +0800

    "version": "1.0.20", 1, Added new method .setNetworkStatusListener to listen for the network status changes 2, Added a browser based testing for NetworkStatusListener 3, Fixed a bug of `clearInterval` in EventPool

--- test/unit/network/NetworkStatus.test.ts
+++ test/unit/network/NetworkStatus.test.ts
@@ -1,6 +1,5 @@
 import { describe, expect } from '@jest/globals';
-import { PublishRequest, PushClient, PushServerResponse, SubscribeRequest, VaPushServerResponse } from "../../../src";
-import { Web3Digester, Web3Signer } from "debeem-id";
+import { PushClient, VaPushServerResponse } from "../../../src";
 import { TestUtil } from "debeem-utils";
 import { testWalletObjList } from "../../../src/configs/TestConfig";
 import _ from "lodash";
@@ -23,44 +22,7 @@ describe( "NetworkStatus", () =>
 	{
 		it( "should receive event continuously", async () =>
 		{
-			let receivedEvents: any[] = [];
-
-			/**
-			 *	@param channel		{string}
-			 *	@param event		{any}
-			 *	@param callback		{( ack : any ) => void}
-			 */
-			const callbackEventReceiver = ( channel : string, event: any, callback: any ) =>
-			{
-				const errorEvent = VaPushServerResponse.validatePushServerResponse( event );
-				if ( null !== errorEvent )
-				{
-					console.warn( `callbackEventReceiver :: [${ channel }] errorEvent :`, errorEvent );
-					return;
-				}
-
-				console.log( `)) 🔔 Client : received event from server: `, event );
-				//	Client : 🔔 received event from server:  {
-				//       timestamp: 1724273039193,
-				//       serverId: '28c726b7-0acf-4102-bd88-810f5494478c',
-				//       version: '1.0.0',
-				//       status: 200,
-				//       data: {
-				//         timestamp: 1724273039193,
-				//         wallet: '0xc8f60eaf5988ac37a2963ac5fabe97f709d6b357',
-				//         channel: 'pch-bobo-0xcbb8f66676737f0423bdda7bb1d8b84fc3c257e8',
-				//         hash: '0x266305d1b1b5157f335ec5b2a9066fbfed38d2f82da82886fe2283add1918fdb',
-				//         sig: '0x979ce436889c0b59b2b94e67c11b4875cd93f403896b150036f98df972d126c4755fe557a86a495c8b2c5b1245672918d69993ae295f72a7352dcacf2de380741c',
-				//         body: { index: 2, time: '8/22/2024, 4:43:59 AM' }
-				//       }
-				//     }
-				//	...
-				receivedEvents.push( event );
-				receivedEvents.sort( ( a, b ) => a.timestamp - b.timestamp );
-			}
-
 			const deviceId = `device-${ testWalletObjList.bob.address }`;
-			const channel = `pch-bobo-${ testWalletObjList.bob.address }`;
 
 			const pushClientOptions = {
 				deviceId : deviceId,
